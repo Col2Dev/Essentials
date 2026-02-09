8a20ce811e14c6306e1071fc8f31d0034d72945b
TactileHornet61 <54224911+Col2Dev@users.noreply.github.com>
2026-02-08 23:01:07 -0500

    Update sidebar and layout configuration
    Refactor layout and sidebar titles in the Streamlit app.

--- main.py
+++ main.py
@@ -19,17 +19,9 @@ def main():
     with st.sidebar:
         st.title("Apostolic Word")
         st.divider()
-
-        options = ["Repent", "Baptism", "Holy Ghost"]
-
-        selection = st.pills(
-            "Current Page:",
-            options,
-            default="Repent"
-        )
-
+        st.title("Bible" )
         st.divider()
-
+    
         books = list(bible.keys())
         book = st.selectbox("Book:", books)
 
@@ -39,10 +31,14 @@ def main():
         chapter = st.selectbox("Chapter:", chapters)
 
     # ---------------- Layout ----------------
-    col1, col2 = st.columns([1.5, 1.5])
+    col1, col2, col3 = st.columns([3, 6,3])
 
     # Bible Column
     with col1:
+        pass
+
+    # Teaching Column
+    with col2:
         st.divider()
         st.markdown("## Bible")
         st.divider()
@@ -54,61 +50,9 @@ def main():
             
             st.write(verse_num + " " + verse_text)
             st.write("")
-
-    # Teaching Column
-    with col2:
-        if selection == "Repent":
-            st.divider()
-            st.markdown("## Acts 2:38")
-            st.divider()
-            st.markdown("""
-**- "Repent** and be **baptized** every one of you in the NAME  
-of Jesus Christ for the remission of sins,  
-and ye shall receive the gift of the Holy Ghost"
-
----
-
-## Repentance
-
-Repentance is the act of acknowledging sins, asking for forgiveness, and turning from them.
-
-It is a state of the soul that is granted by God.
-
-It is a death of the old man.
-""")
-            st.divider()
-
-        elif selection == "Baptism":
-            st.divider()
-            st.markdown("## Baptism")
-            st.divider()
-            st.markdown("""
-Baptism is a symbol of burial.
-
-You bury the old man which washes away your past self, and makes you a new man.
-
-It is a remission of sins.
-""")
-            st.divider()
-
-        elif selection == "Holy Ghost":
-            st.divider()
-            st.markdown("## Holy Ghost")
-            st.divider()
-            st.markdown("""
-The Gift of the Holy Ghost is the Lord's Spirit coming down from heaven and living inside you.
-
-You will know He is living in you when you begin to speak in an unknown language according to Acts 2:4.
-
-The Lord's Spirit gives power over:
-
-• Flesh  
-• Devils  
-• And more
-""")
-            st.divider()
-
+            
+    with col3: 
+        pass
 #RUN APP
 if __name__ == "__main__":
     main()
-
